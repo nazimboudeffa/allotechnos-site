7477c79caf09393c692717e896b65a87ef759f73
Nazim Boudeffa <boudeffa@gmail.com>
2022-05-31 13:30:59 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/public'))
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+let port = 3000;
+
+app.listen(port, function () {
+  console.log('AlloTechnos app listening on port 3000!')
+})
